ee46dcf20eac5df58bd53ccef3fbb0bcf47d5080
Mgazul <mgazul@163.com>
2020-11-30 04:23:12 +0800

    Remove obsolete tools

--- src/main/java/red/mohist/mohistremaptools/CraftBukkitPatches2MCP.java
+++ src/main/java/red/mohist/mohistremaptools/CraftBukkitPatches2MCP.java
@@ -1,10 +1,10 @@
 package red.mohist.mohistremaptools;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,7 +39,7 @@ public class CraftBukkitPatches2MCP {
         StringBuilder sb = new StringBuilder();
         cl.forEach((key, value) -> sb.append(key).append(" - ").append(value).append("\n"));
         File file = new File("cache", "cb2mcp_patches.srg");
-        Spigot2Mcp.writeByteArrayToFile(file, sb);
+        writeByteArrayToFile(file, sb);
         System.out.println("成功保存" + cl.size() + "条数据");
 
         // rename
@@ -84,5 +84,13 @@ public class CraftBukkitPatches2MCP {
             }
         }
         return fileList;
+    }
+
+    public static void writeByteArrayToFile(File file, StringBuilder sb){
+        try {
+            FileUtil.writeData(file, sb.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
